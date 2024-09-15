9c4a4fcdc692124f7559c1881cc35e8506cb4418
Fin M <qbres333@hotmail.com>
2024-09-14 18:39:39 -0600

    edit index route and server files

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 // Import custom middleware
 const { logger } = require("../middleware/logger");
 
-// Import modular routers for /notes
+// Import modular router for /notes
 const notesRouter = require("./notes");
 
 const app = express();
--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,27 @@ const express = require("express");
 const path = require("path");
 const { logger } = require("../middleware/logger");
 const api = require("./routes/index.js");
+
+const PORT = 3001;
+const app = express();
+
+// middleware for parsing JSON
+app.use(express.json());
+app.use("/api", api);
+app.use(logger);
+// tell the app to serve static files from the public directory
+app.use(express.static("public"));
+
+// GET route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(_dirname, "/public/index.html"))
+);
+
+// GET route for notes page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(_dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
